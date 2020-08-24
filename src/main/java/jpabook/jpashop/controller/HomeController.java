1c39926aa5b2f50a2c2965dc8b12ba4122ad1b92
이승환 <lshk955@naver.com>
2020-08-24 15:45:05 +0900

    화면레이아웃, 회원 등록 추가

---
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.controller;
+
+        import lombok.extern.slf4j.Slf4j;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 이승환
+ * @since 2020-08-24
+ */
+@Controller
+@Slf4j
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+}
