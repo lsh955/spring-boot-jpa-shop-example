b8cfbba80637e77a2463b3b0a4e4904a4086a05c
이승환 <lshk955@naver.com>
2020-08-25 11:28:13 +0900

    상품 주문, 주문 목록 검색/취소 추가

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -8,6 +8,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -74,11 +75,12 @@ public class OrderService {
 
     /**
      * 검색
+     *
      * @param orderSearch
      * @return
      */
-//    public List<Order> findOrders(OrderSearch orderSearch) {
-//        return orderRepository.findOne(orderSearch);
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
+    }
 
 }
